8b403f96d54e3dd7554383d5b5635bf3d7e263cb
SENA <SENA@10.12.6.98>
2017-04-26 09:04:32 -0500

    A todos nos gusta...fff

---
+++ Clase 2 java/src/clase/pkg2/java/Clase2Java.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase.pkg2.java;
+
+public class Clase2Java
+{
+    private String direction;
+    private String pet;
+    
+  //Set all
+    
+   public void setDirection(String directionPara)
+   {
+       this.direction= directionPara;
+   }
+   
+   public void setPet (String petPara)
+   {
+       this.pet= petPara;
+   }
+    
+   //get all
+    
+    public String getDirection()
+    {
+       return this.direction;
+    }
+    
+    public String getPet()
+    {
+        return this.pet;
+    }
+    
+  public static void main (String[] args)    
+    {
+       
+        
+        
+        
+        
+        
+    }
+   
+
+}
